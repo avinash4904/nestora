a9b43e97ab050b9d4efb14c2718045f6754d3cf7
avinash4904 <022avinashgupta@gmail.com>
2025-11-13 18:14:23 +0530

    update CORS configuration to allow any origin

--- backend/index.js
+++ backend/index.js
@@ -14,9 +14,12 @@ let app = express()
 app.use(express.json())
 app.use(cookieParser())
 app.use(cors({
-    origin:"http://localhost:5173",
-    credentials:true
-}))
+  origin: (origin, callback) => {
+    callback(null, true);  // allow ANY origin
+  },
+  credentials: true
+}));
+
 
 app.use("/api/auth", authRouter )
 app.use("/api/user", userRouter )
